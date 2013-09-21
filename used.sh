8ce27717584ebdebe4d8ff1e289aa21795bd0c65
Jean Guyomarc'h <jeanguyomarch.github@gmail.com>
2013-09-21 18:16:20 +0200

    Cleanup: remove unused resources

---
+++ used.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+#
+# Determines if there are unused files in the theme.
+#
+
+IGNORES="LICENSE.md Makefile README.md images pikalogy pikalogy.edc pikalogy.edj sounds used.sh"
+
+dirs=". images sounds"
+for d in $dirs; do
+   cd "$d"
+   files=`ls`
+   for l in $files; do
+      ignore=0
+
+      for i in $IGNORES; do
+         if [ x"$l" == x"$i" ]; then
+            ignore=1
+            break
+         fi
+      done
+
+      if [ $ignore -eq 1 ]; then
+         continue
+      fi
+
+      grep -n "$l" ../pikalogy.edc 2>&1 > /dev/null
+      if [ $? -ne 0 ]; then
+         echo "UNUSED IN EDJE: $l"
+      fi
+   done
+   cd - 2>&1 > /dev/null
+done
+
